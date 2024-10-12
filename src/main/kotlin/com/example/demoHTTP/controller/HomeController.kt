25344eb248427af1a57dac47cb2aaa313acd926d
Alexnarea <lanarea@sudamericano.edu.ec>
2024-10-12 10:12:25 -0500

    update pathParam

--- src/main/kotlin/com/example/demoHTTP/controller/HomeController.kt
+++ src/main/kotlin/com/example/demoHTTP/controller/HomeController.kt
@@ -1,15 +1,20 @@
 package com.example.demoHTTP.controller
 
+import jakarta.websocket.server.PathParam
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/home")
 class HomeController {
-    @GetMapping("/{id}")
+    /*@GetMapping("/{id}")
     fun home(@PathVariable ("id") id: Int): String {
+        return "Su ID es:  ${id}"
+    }*/
+
+    @GetMapping()
+    fun home(@PathParam ("id") id: Int): String {
         return "Su ID es:  ${id}"
     }
 
