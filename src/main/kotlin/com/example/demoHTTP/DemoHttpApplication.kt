99485e56fee2f5f6c9a2e3532086281ae0968289
Alexnarea <lanarea@sudamericano.edu.ec>
2024-10-12 09:41:29 -0500

    first

---
+++ src/main/kotlin/com/example/demoHTTP/DemoHttpApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demoHTTP
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoHttpApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoHttpApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demoHTTP/DemoHttpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demoHTTP
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoHttpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
