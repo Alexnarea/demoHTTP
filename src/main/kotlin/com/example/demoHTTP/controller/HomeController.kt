57a75e0ca03306eb13c3ffb13aa8743f6fb29b3d
Alexnarea <lanarea@sudamericano.edu.ec>
2024-10-12 10:33:10 -0500

    update RequestBody

--- src/main/kotlin/com/example/demoHTTP/controller/HomeController.kt
+++ src/main/kotlin/com/example/demoHTTP/controller/HomeController.kt
@@ -1,7 +1,8 @@
 package com.example.demoHTTP.controller
 
-import jakarta.websocket.server.PathParam
+import com.example.demoHTTP.model.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,9 +14,13 @@ class HomeController {
         return "Su ID es:  ${id}"
     }*/
 
-    @GetMapping()
+    /*@GetMapping()
     fun home(@PathParam ("id") id: Int): String {
         return "Su ID es:  ${id}"
-    }
+    }*/
 
+    @GetMapping()
+    fun home(@RequestBody model: Model): String {
+        return "Su ID es: ${model.id} su nombre es ${model.name} su correo electronico es ${model.mail} y tiene ${model.age} años"
+    }
 }
---
+++ src/main/kotlin/com/example/demoHTTP/model/Model.kt
@@ -0,0 +1,8 @@
+package com.example.demoHTTP.model
+
+class Model {
+    var id: Int = 0
+    var name: String = ""
+    var mail: String = ""
+    var age: Int = 0
+}
